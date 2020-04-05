e52e30545225426be283eec862a3839645e9ef7e
nirrvana <wlwhs91@gmail.com>
2020-04-05 15:02:07 +0900

    Solve

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -3,6 +3,4 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const twoFer = (someone = 'you') => `One for ${someone}, one for me.`;
