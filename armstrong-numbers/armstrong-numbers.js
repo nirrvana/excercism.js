38f55533e69b12e4036c43392abbf1be5e236982
nirrvana <wlwhs91@gmail.com>
2020-04-09 21:42:58 +0900

    Solve

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -3,6 +3,13 @@
 // convenience to get you started writing code faster.
 //
 
-export const isArmstrongNumber = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isArmstrongNumber = (num) => {
+  const str = num.toString();
+  let sum = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    sum += Math.pow(Number(str[i]), str.length);
+  }
+
+  return num === sum;
 };
