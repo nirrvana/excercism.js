5138d9713b7a691badb03a278bade6e11ff6838e
nirrvana <wlwhs91@gmail.com>
2020-04-11 10:37:49 +0900

    Solve

--- darts/darts.js
+++ darts/darts.js
@@ -3,6 +3,20 @@
 // convenience to get you started writing code faster.
 //
 
-export const score = () => {
-  throw new Error("Remove this statement and implement this function");
+const INNER = 1;
+const MIDDLE = 5;
+const OUTER = 10;
+
+export const score = (x, y) => {
+  const length = Math.sqrt(x * x + y * y);
+
+  if (length <= INNER) {
+    return 10;
+  } else if (length <= MIDDLE) {
+    return 5;
+  } else if (length <= OUTER) {
+    return 1;
+  } else {
+    return 0;
+  }
 };
