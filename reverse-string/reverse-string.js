6c919fb507ee5e2d05b67d954a48d0042b916fc6
nirrvana <wlwhs91@gmail.com>
2020-04-16 14:48:58 +0900

    Solve

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -3,6 +3,12 @@
 // convenience to get you started writing code faster.
 //
 
-export const reverseString = () => {
-  throw new Error("Remove this statement and implement this function");
+export const reverseString = (string) => {
+  let result = '';
+
+  for (let index = string.length - 1; index >= 0; index--) {
+    result += string[index];
+  }
+
+  return result;
 };
