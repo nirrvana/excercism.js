b56fa907ee5c257098bc27adb55704759c019a36
nirrvana <wlwhs91@gmail.com>
2020-04-16 15:12:03 +0900

    Solve

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,14 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+export const steps = (num, count = 0) => {
+  if (num <= 0) {
+    throw new Error('Only positive numbers are allowed');
+  } else if (num === 1) {
+    return count;
+  } else if (num % 2 === 0) {
+    return steps(num / 2, ++count);
+  } else {
+    return steps(num * 3 + 1, ++count);
+  }
 };
