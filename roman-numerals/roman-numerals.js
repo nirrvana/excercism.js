026b64c4885b31b3bbc40e81c448b1a3e7f65e5c
nirrvana <wlwhs91@gmail.com>
2020-05-27 00:06:16 +0900

    Solve

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -3,6 +3,33 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRoman = () => {
-  throw new Error("Remove this statement and implement this function");
+export const toRoman = (number) => {
+  let result = '';
+
+  while (number > 0) {
+    for (const ROMAN in ROMAN_NUMBER_TABLE) {
+      if (ROMAN_NUMBER_TABLE[ROMAN] <= number) {
+        result += ROMAN;
+        number -= ROMAN_NUMBER_TABLE[ROMAN];
+        break;
+      }
+    }
+  }
+
+  return result;
+};
+const ROMAN_NUMBER_TABLE = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
 };
